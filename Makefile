b0d09cfa660483eba76a33c7619572f85e3c2b51
Qi-Zhan <3190103512@zju.edu.cn>
2023-08-09 13:43:12 +0800

    update makefile

--- Makefile
+++ Makefile
@@ -1,23 +1,12 @@
-.PHONY: refresh build install release test clean
+.PHONY: install test clean
 
-refresh: clean build install
-
-build:
-	python -m build
-
-install: build
-	pip install dist/*.whl
-
-release:
-	python -m twine upload dist/*
+install: 
+	pip install -e .
 
 test:
 	python -m unittest
 
 clean:
-	rm -rf __pycache__
-	rm -rf tests/__pycache__
-	rm -rf src/watchyourself/__pycache__
-	rm -rf dist
-	rm -rf src/watchyourself.egg-info
-	pip uninstall -y watchyourself
+	rm -rf **__pycache__**
+	rm -rf src/tipy/__pycache__
+	rm -rf src/tipy.egg-info
--- src/tipy/solver/unionfind.py
+++ src/tipy/solver/unionfind.py
@@ -1,7 +1,38 @@
 from .solver import Solver
+from ..util import TypeError
+
+
+def make_set(x):
+    """
+    adds a new node x that is its own parent
+    """
+    x.parent = x
+
+
+def find(x):
+    """
+    returns the representative of the set that x belongs to
+    """
+    if x.parent != x:
+        x.parent = find(x.parent)
+    return x.parent
+
+
+def union(x, y):
+    """
+    merges the set containing x and the set containing y
+    """
+    x_root = find(x)
+    y_root = find(y)
+    if x_root != y_root:
+        x_root.parent = y_root
 
 
 class UnionFindSolver(Solver):
     @classmethod
     def solve(cls, constraints):
         print("UnionFindSolver.solve")
+
+        for constraint in constraints:
+            make_set(constraint.left)
+            make_set(constraint.right)
